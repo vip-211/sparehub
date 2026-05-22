24918b0b1cadf9eb16e50a592d94efa0b5aa489b
Vipin <vipinchaurasiya1052@gmail.com>
2026-05-22 14:05:26 +0530

    Fix: admin login

--- backend/src/main/java/com/spareparts/inventory/dto/LoginRequest.java
+++ backend/src/main/java/com/spareparts/inventory/dto/LoginRequest.java
@@ -1,12 +1,14 @@
 
 package com.spareparts.inventory.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     @NotBlank
+    @JsonAlias({"email", "username"})
     private String identifier;
 
     @NotBlank
